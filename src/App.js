4f463c529a9e9357b7204e1ec2aeefe3f7ecb70b
LeoniePeters <leoniepeters@gmail.com>
2019-01-21 15:44:33 +0100

    create Title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title'
 
 class App extends Component {
   render() {
@@ -21,6 +22,10 @@ class App extends Component {
             Learn React
           </a>
         </header>
+
+        <main>
+          <Title />
+        </main>
       </div>
     );
   }
